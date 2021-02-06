d11f4019a1083a3220c5462ecda720ef1ad942f6
Daniel <dannielmdr7@gmail.com>
2021-02-06 07:42:47 -0500

    RestServer- Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,18 @@
+//==========PUERTO==========
 process.env.PORT = process.env.PORT || 3000;
+
+//==========ENTORNO==========
+process.env.NODE_ENV=process.env.NODE_ENV || 'dev'
+
+
+//============Base de Datos======
+
+let urlDB
+
+// if ( process.env.NODE_ENV ==='dev'  ) {
+//     urlDB='mongodb://localhost:27017/cafe'
+// }else{
+    urlDB='mongodb+srv://Daniel:XyK4RuR5L8nKPFKh@cluster0.itb4c.mongodb.net/test'
+// }
+
+process.env.urlDB=urlDB;
--- server/server.js
+++ server/server.js
@@ -1,45 +1,26 @@
-const { urlencoded } = require('body-parser');
+
 
 require('./config/config')
 
 const express=require('express');
+const mongoose=require('mongoose');
+
+
+
 const app=express();
-const bodyParser=require('body-parser');
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
+app.use(require('./routes/usuario')); 
 
-app.get('/usuario',(req,res)=>{
-    res.json('GET funcionando');
+mongoose.connect(process.env.urlDB,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex:true,
+        useFindAndModify:false},(err,res)=>{
+    
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+
 });
-app.post('/usuario',(req,res)=>{
-    let body=req.body;
-    if(body.nombre ===undefined ){
-        res.status(400).json({
-            ok:false,
-            messaje:'El nombre es obligatorio'
-        })
-
-    }else{
-        res.json({
-            body
-        })
-
-    }
-})
-app.put('/usuario/:id',(req,res)=>{
-    let id =req.params.id;
-    res.json({
-        id
-    })
-})
-app.delete('/usuario',(req,res)=>{
-    res.json('Hola desde el DELETE')
-})
-
 
 app.listen(process.env.PORT,()=>{
     console.log('escuchando en el puerto ',process.env.PORT);
